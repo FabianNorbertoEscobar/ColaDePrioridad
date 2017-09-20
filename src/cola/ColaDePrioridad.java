2ca5edcafa9cd697cfad728627cfc0e9affcc07f
Fabian Escobar <fabiannorbertoescobar@gmail.com>
2017-09-20 20:56:58 -0300

    Se arranca con la cola

--- src/cola/ColaDePrioridad.java
+++ src/cola/ColaDePrioridad.java
@@ -1,6 +1,34 @@
 package cola;
 
+import monticulo.Monticulo;
+import monticulo.MonticuloMinimo;
+
 public class ColaDePrioridad {
 
+	private Monticulo monticulo;
+	private int tamañoMaximo;
+	private int cantidadDeElementos;
+
+	public int getTamañoMaximo() {
+		return tamañoMaximo;
+	}
+
+	public int getCantidadDeElementos() {
+		return cantidadDeElementos;
+	}
+
+	public ColaDePrioridad(int tamañoMaximo) {
+		this.tamañoMaximo = tamañoMaximo;
+		this.monticulo = new MonticuloMinimo(tamañoMaximo);
+		this.cantidadDeElementos = 0;
+	}
+
+	public boolean estaVacia() {
+		return this.monticulo.estaVacio();
+	}
+
+	public boolean estaLlena() {
+		return this.estaLlena();
+	}
 	
 }
