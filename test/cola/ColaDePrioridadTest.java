957c48467b09f702dca63023ae43ef79881abb79
Fabian Escobar <fabiannorbertoescobar@gmail.com>
2017-09-20 21:52:36 -0300

    Test de insertar, eliminar y pispear OK

--- test/cola/ColaDePrioridadTest.java
+++ test/cola/ColaDePrioridadTest.java
@@ -22,4 +22,35 @@ public class ColaDePrioridadTest {
 	public void estaVacio() {
 		Assert.assertTrue(this.cola.estaVacia());
 	}
+	
+	@Test
+	public void tamañoMaximo() {
+		Assert.assertEquals(10, this.cola.getTamañoMaximo());
+	}
+	
+	@Test
+	public void cantidadDeElementos() {
+		Assert.assertEquals(0, this.cola.getCantidadDeElementos());
+	}
+	
+	@Test
+	public void queInserta() {
+		this.cola.insertar(5);
+		Assert.assertEquals(1, this.cola.getCantidadDeElementos());
+	}
+	
+	@Test
+	public void queElimina() {
+		this.cola.insertar(5);
+		Assert.assertEquals(5, this.cola.eliminar());
+		Assert.assertEquals(0, this.cola.getCantidadDeElementos());
+	}
+	
+	@Test
+	public void quePispea() {
+		this.cola.insertar(5);
+		Assert.assertEquals(5, this.cola.pispear());
+		Assert.assertEquals(1, this.cola.getCantidadDeElementos());
+	}
+	
 }
