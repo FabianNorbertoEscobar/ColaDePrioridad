8d8f822819f9fcc3a8182aa0379831797e4b19bb
Fabian Escobar <fabiannorbertoescobar@gmail.com>
2017-09-20 21:09:34 -0300

    Se agregan los métodos insertar, eliminar y pispear

--- src/cola/ColaDePrioridad.java
+++ src/cola/ColaDePrioridad.java
@@ -30,5 +30,27 @@ public class ColaDePrioridad {
 	public boolean estaLlena() {
 		return this.estaLlena();
 	}
-	
+
+	public void insertar(int nuevo) throws IllegalStateException {
+		if (this.monticulo.estaLleno()) {
+			throw new IllegalStateException("No se puede insertar. Cola de prioridad llena.");
+		}
+		this.cantidadDeElementos++;
+		this.insertar(nuevo);
+	}
+
+	public int eliminar() throws IllegalStateException {
+		if (this.monticulo.estaVacio()) {
+			throw new IllegalStateException("No se puede eliminar. Cola de prioridad vacía.");
+		}
+		this.cantidadDeElementos--;
+		return this.eliminar();
+	}
+
+	public int pispear() {
+		if (this.monticulo.estaVacio()) {
+			throw new IllegalStateException("No se puede pispear. Cola de prioridad vacía.");
+		}
+		return this.pispear();
+	}
 }
