e62d4bb332cc4354b8e6aeb046d30ff78df1abbc
Fabian Escobar <fabiannorbertoescobar@gmail.com>
2017-09-20 22:45:37 -0300

    Se testean los métodos insertar, eliminar y pispear -- Todo OK

--- test/cola/ColaDePrioridadTest.java
+++ test/cola/ColaDePrioridadTest.java
@@ -1,5 +1,7 @@
 package cola;
 
+import java.util.Random;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -53,4 +55,36 @@ public class ColaDePrioridadTest {
 		Assert.assertEquals(1, this.cola.getCantidadDeElementos());
 	}
 	
+	@Test
+	public void queSeRespetaLaPrioridad() {
+		this.cola.insertar(5);
+		this.cola.insertar(2);
+		this.cola.insertar(4);
+		this.cola.insertar(1);
+		this.cola.insertar(3);
+		Assert.assertEquals(1, this.cola.eliminar());
+		Assert.assertEquals(2, this.cola.eliminar());
+		Assert.assertEquals(3, this.cola.eliminar());
+		Assert.assertEquals(4, this.cola.eliminar());
+		Assert.assertEquals(5, this.cola.eliminar());
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void queSeLLena() {
+		Random rand = new Random();
+		while (true) {
+			this.cola.insertar(rand.nextInt(100));
+		}
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void queSeVacia() {
+		Random rand = new Random();
+		for (int i = 0; i < 10; i++) {
+			this.cola.insertar(rand.nextInt(100));
+		}
+		while (true) {
+			this.cola.eliminar();
+		}
+	}
 }
